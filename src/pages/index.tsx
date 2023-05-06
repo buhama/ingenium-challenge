48f4db2ca37961e29365996fb954bbff4f493e5c
buhama <hassan@hassanahmed.ca>
2023-05-06 13:43:39 -0400

    remvoe improts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "../../styles/Home.module.css";
-import { Button, Input } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { supabase } from "../../supabaseClient";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
