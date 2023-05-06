e2ebe68a66f710552a6758ad31c7915b360d4c06
buhama <hassan@hassanahmed.ca>
2023-05-06 13:43:12 -0400

    redirect to login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,40 +3,17 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../../styles/Home.module.css";
 import { Button, Input } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../../supabaseClient";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const { data, error } = await supabase.auth.signUp({
-      email,
-      password,
-    })}
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/login");
+  }, []);
 
-  return (
-    <>
-      <form onSubmit={submit}>
-        <Input
-          type="email"
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button colorScheme="blue" type="submit">
-          Sign Up
-        </Button>
-      </form>
-    </>
-  );
+  return <>you shouldnt be here</>;
 }
