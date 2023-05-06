506d8633283dafa269a80fd6c2bb1f287d35c9b1
buhama <hassan@hassanahmed.ca>
2023-05-06 11:20:09 -0400

    pls

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
+import React, { useState } from "react";
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { NextPage } from "next";
+import { ReactElement, ReactNode, useEffect } from "react";
+import theme from "../../theme";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <ChakraProvider theme={theme}>
+      {getLayout(<Component {...pageProps} />)}
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
---
+++ theme.js
@@ -0,0 +1,43 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const themes = {
+  colors: {
+    yellow: {
+      50: "#fffada",
+      100: "#fff0ad",
+      200: "#ffe67d",
+      300: "#ffdb4b",
+      400: "#ffd11a",
+      500: "#e6b800",
+      600: "#b38f00",
+      700: "#806600",
+      800: "#4e3d00",
+      900: "#1d1400",
+    },
+  },
+  components: {
+    Modal: {
+      baseStyle: {
+        dialog: {
+          mx: "2",
+        },
+      },
+    },
+    Divider: {
+      baseStyle: {
+        borderColor: "yellow.500",
+      },
+    },
+    Popover: {
+      baseStyle: {
+        popper: {
+          width: "fit-content",
+          maxWidth: "fit-content",
+        },
+      },
+    },
+  },
+};
+const theme = extendTheme(themes);
+
+export default theme;
